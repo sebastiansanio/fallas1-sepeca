64c6876a33fe30d9833927d01592af485205b204
seba90s@gmail.com <seba90s@gmail.com@1f5c2bc6-96be-7b56-9214-72d5aa240606>
2012-05-27 01:32:57 +0000

    Pequeño ejemplo con Drools.

---
+++ trunk/fallas1/src/main/Car.java
@@ -0,0 +1,30 @@
+package main;
+
+public class Car {
+
+    private String brand;
+    private String model;
+    
+	public Car() {
+
+    }
+
+    public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+
+    public String getBrand() {
+        return this.brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+}
+    
+    
