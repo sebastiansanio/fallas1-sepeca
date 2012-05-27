8b92cfc69f7bf799c5c051b08f3c5b7f985010c5
seba90s <seba90s@gmail.com>
2012-05-27 01:32:57 +0000

    Pequeño ejemplo con Drools.

--- fallas1/src/main/Main.java
+++ fallas1/src/main/Main.java
@@ -1,10 +1,55 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.drools.KnowledgeBase;
+import org.drools.KnowledgeBaseFactory;
+import org.drools.builder.KnowledgeBuilder;
+import org.drools.builder.KnowledgeBuilderFactory;
+import org.drools.builder.ResourceType;
+import org.drools.definition.KnowledgePackage;
+import org.drools.event.rule.DebugAgendaEventListener;
+import org.drools.event.rule.DebugWorkingMemoryEventListener;
+import org.drools.io.ResourceFactory;
+import org.drools.runtime.StatefulKnowledgeSession;
+
+
 public class Main {
 
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		final KnowledgeBuilder kbuilder = KnowledgeBuilderFactory.newKnowledgeBuilder();
+		kbuilder.add( ResourceFactory.newClassPathResource( "Car.drl",Car.class ),ResourceType.DRL );
+
+		if ( kbuilder.hasErrors() ) {
+			throw new RuntimeException( "Unable to compile \"Car.drl\"." );
+		}
+		
+		final Collection<KnowledgePackage> pkgs = kbuilder.getKnowledgePackages();
+
+		final KnowledgeBase kbase = KnowledgeBaseFactory.newKnowledgeBase();
+		kbase.addKnowledgePackages( pkgs );
+
+		final StatefulKnowledgeSession ksession = kbase.newStatefulKnowledgeSession();
+		ksession.setGlobal( "list",new ArrayList<Object>() );
+
+		ksession.addEventListener( new DebugAgendaEventListener() );
+		ksession.addEventListener( new DebugWorkingMemoryEventListener() );
+
+		final Car car = new Car();
+		car.setBrand( "Fiat" );
+		car.setModel("Palio");
+		
+		final Car car2 = new Car();
+		car2.setBrand( "Ford" );
+		car2.setModel("Fiesta");
+		
+		ksession.insert(car);
+		ksession.insert(car2);
+		ksession.fireAllRules();
+		ksession.dispose();
 
 	}
 
